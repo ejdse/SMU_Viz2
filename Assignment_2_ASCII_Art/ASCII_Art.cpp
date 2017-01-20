92d14eb707ac65f553dd76e20a6fb699a52f49ba
Andrew Clark <brassatc@icloud.com>
2017-01-19 20:54:01 -0500

    new repo, initial commit

---
+++ Assignment_2_ASCII_Art/ASCII_Art.cpp
@@ -0,0 +1,34 @@
+// Assignment 2: ASCII Art
+// Generate an image only using C++'s cout.
+// Andrew Clark and Ryan Shuhart
+
+// References:
+// http://www.cplusplus.com/forum/general/58945/
+
+#include <iostream>
+#include <string> 
+using namespace std; 
+
+// COPIED EXAMPLE. CHANGE
+
+int main () 
+{
+	cout<<"#############################################################"<<endl;
+	cout<<"#                    _                                      #"<<endl;  
+	cout<<"#                  -=\\`\\                                    #"<<endl;  
+	cout<<"#              |\\ ____\\_\\__                                 #"<<endl;  
+	cout<<"#            -=\\c`""""""" "`)                               #"<<endl;  
+	cout<<"#               `~~~~~/ /~~`\                                #"<<endl;  
+	cout<<"#                 -==/ /                                    #"<<endl;  
+	cout<<"#                   '-'                                     #"<<endl;  
+	cout<<"#                  _  _                                     #"<<endl;  
+	cout<<"#                 ( `   )_                                  #"<<endl;  
+	cout<<"#                (    )    `)                               #"<<endl;  
+	cout<<"#              (_   (_ .  _) _)                             #"<<endl;  
+	cout<<"#                                             _             #"<<endl;  
+	cout<<"#                                            (  )           #"<<endl;  
+	cout<<"#             _ .                         ( `  ) . )        #"<<endl;  
+	cout<<"#           (  _ )_                      (_, _(  ,_)_)      #"<<endl;  
+	cout<<"#         (_  _(_ ,)                                        #"<<endl;  
+	cout<<"#############################################################"<<endl;
+}
