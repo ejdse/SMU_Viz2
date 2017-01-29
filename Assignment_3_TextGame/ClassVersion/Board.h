27f436a21d6c67ead09a06ccd798a1bfbfa30e12
Andrew Clark <brassatc@icloud.com>
2017-01-28 21:28:53 -0500

    assignment 3

---
+++ Assignment_3_TextGame/ClassVersion/Board.h
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Board {
+
+public:
+
+  char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+
+void board()
+{
+	system("cls");
+	cout << "\n\n\tTic Tac Toe\n\n";
+
+	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << endl;
+
+	cout << "     |     |     " << endl;
+	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+
+	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+
+	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+
+	cout << "     |     |     " << endl << endl;
+  }
+};
---
+++ Assignment_3_TextGame/ClassVersion/main.cpp
@@ -0,0 +1,6 @@
+// Resources:
+// http://www.cppforschool.com/project/tic-tac-toe-project.html
+// http://codereview.stackexchange.com/questions/123200/tic-tac-toe-c
+#include "Board.h"
+
+Board testing;
